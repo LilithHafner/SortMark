4a9cf1282b1afc33eb0458af3854cee7445efa16
Documenter.jl <documenter@juliadocs.github.io>
2021-11-06 23:42:02 +0000

    build based on 43fbf1f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SortMark","category":"page"},{"location":"#SortMark","page":"Home","title":"SortMark","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SortMark.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SortMark]","category":"page"}]
+}
